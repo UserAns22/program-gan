dc83dddc0dd7bcefddd8275436abd4a2f8710f34
UserAns22 <aniapplefan@gmail.com>
2017-11-04 16:27:59 -0700

    Changed the grammer function to output rational functions of degree three or less

--- grammar_tree.py
+++ grammar_tree.py
@@ -1,16 +1,17 @@
+from random import randint, choice
+
 # Possible mutations to initial function
-DATASET_MUTATIONS = ["+1", "-1", "+x", "-x", "*(x+1)", "/(x+1)"]
+DATASET_MUTATIONS = ["({} * x + {})", "({} * (x ** 2) + {})", "{}"]
 print("Allowed mutations:", DATASET_MUTATIONS)
 
 
 # Number of operations to chain
-RECURSION_DEPTH = 10
-print("Grammar tree depth:", RECURSION_DEPTH)
+NUM_EXAMPLES = 1000000
+print("Examples:", NUM_EXAMPLES)
+DIMENSION = 3
 
 
 # Track number of entries in dataset file
-DATASET_LENGTH = sum([len(DATASET_MUTATIONS)**(i + 1) for i in range(RECURSION_DEPTH)])
-print("Total number rows:", DATASET_LENGTH)
 
 
 # Initial program to mutate from
@@ -23,7 +24,7 @@ DATASET_IO_EXAMPLES = 10
 
 
 # Path to dataset file
-DATASET_FILEPATH = ("epf_" + str(RECURSION_DEPTH) + "_dataset.csv")
+DATASET_FILEPATH = ("rpf_" + str(DIMENSION) + "_dataset.csv")
 
 
 # Open dataset file write stream
@@ -64,25 +65,55 @@ def possible_mutations(current_program):
 
 
 # Recursively mutate programs using tree
-def mutate_program(current_program, recursive_depth):
+# def mutate_program(current_program, recursive_depth):
+#     
+#     # Check if enough mutations have been made
+#     if recursive_depth > 0:
+# 
+#         # Generate program for every allowed mutation
+#         for mutation in possible_mutations(current_program):
+# 
+#             # Mutate existing code
+#             new_program = current_program + mutation
+# 
+# 
+#             # Mutate already mutated code
+#             mutate_program(new_program, recursive_depth - 1)
+# 
+# 
+#             # Save mutated code as program example
+#             render_program(new_program)
+# 
+
+# Programs are a ratio of two 0 to 3 dimensional polynomials
+
+def piece():
+    r = randint(0, 2)
+    s = DATASET_MUTATIONS[r]
+    constants = list(range(-9,10))
+
+    if r != 2:
+        c1,c2 = choice(constants), choice(constants)
+        while c1 ==0 and c2 == 0:
+            c1,c2 = choice(constants), choice(constants)
+        return s.format(c1, c2)
+    constants.remove(0)
+    return s.format(choice(constants))
+
+
+def ratl_func(num_dim, denom_dim):
+    numer = poly(num_dim)
+    denom = poly(denom_dim)
+    render_program("lambda x: " + numer + " / " + denom)
     
-    # Check if enough mutations have been made
-    if recursive_depth > 0:
-
-        # Generate program for every allowed mutation
-        for mutation in possible_mutations(current_program):
-
-            # Mutate existing code
-            new_program = current_program + mutation
-
-
-            # Mutate already mutated code
-            mutate_program(new_program, recursive_depth - 1)
-
-
-            # Save mutated code as program example
-            render_program(new_program)
-
+def poly(dim):
+    st = "(1"
+    for i in range(dim):
+        st += ('*' + piece())
+    st += ')'
+    return st
+    
+    
 
 # Function to render dataset line with multiprocessing
 def render_program(current_program):
@@ -101,8 +132,26 @@ def render_program(current_program):
 
 
     # Encode columns two through twenty-one: IO examples
-    for n in range(DATASET_IO_EXAMPLES):
-        current_line += str(n) + DATASET_DELIMINATOR + str(F(n)) + DATASET_DELIMINATOR
+    exp = []
+    i = 0
+    j = 0
+    while i < DATASET_IO_EXAMPLES:
+        n = randint(-20,20)
+        if n not in exp:
+            try:
+                current_line += str(n) + DATASET_DELIMINATOR + str(F(n)) + DATASET_DELIMINATOR
+                i += 1
+                j = 0
+                exp.append(n)
+            except(ZeroDivisionError):
+                n = randint(-20,20)
+                j += 1
+                if j > 10000:
+                    print('ruh roh')
+                    print(current_program, "\n", n)
+                    j = 0
+
+
 
 
     # Encode final column: source code
@@ -120,5 +169,9 @@ clear_programs()
 
 # Mutate existing program recursively, and generate tree
 print("Generating grammar tree.")
-mutate_program(DATASET_SEED, RECURSION_DEPTH)
+# mutate_program(DATASET_SEED, RECURSION_DEPTH)
+
+for i in range(NUM_EXAMPLES):
+    ratl_func(DIMENSION, DIMENSION)
+
 print("Finished.")
